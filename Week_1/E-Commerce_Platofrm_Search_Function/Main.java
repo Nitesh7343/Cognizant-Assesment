fb2d458e9b5ce178317547146b7558e41fe33f7a
Nitesh7343 <niteshsingh7343@gmail.com>
2026-06-21 22:05:48 +0530

    main class for E commerce platform

---
+++ Week_1/E-Commerce_Platofrm_Search_Function/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main methode");
+    }
+
+    public static Product LinearSearch() {
+        return new Product(1,"3","3");
+    }
+}
