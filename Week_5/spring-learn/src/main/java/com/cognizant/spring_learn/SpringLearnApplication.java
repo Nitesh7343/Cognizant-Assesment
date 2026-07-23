26b8e6533c5a395f7c880e06963e311ce7e7e7dc
Nitesh7343 <niteshsingh7343@gmail.com>
2026-07-23 15:41:39 +0530

    Display country function implemented and ex2 completed

--- Week_5/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ Week_5/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -1,5 +1,6 @@
 package com.cognizant.spring_learn;
 
+import com.cognizant.spring_learn.model.Country;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,11 +9,18 @@ import java.util.Date;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SpringBootApplication
 public class SpringLearnApplication implements CommandLineRunner {
 
+	private static final Logger LOGGER =
+			LoggerFactory.getLogger(SpringLearnApplication.class);
+
 	public void run(String... args) throws Exception {
-		displayDate();
+		//displayDate();
+		displayCountry();
 	}
 
 	public static void main(String[] args) {
@@ -25,6 +33,16 @@ public class SpringLearnApplication implements CommandLineRunner {
 
 		Date date = format.parse("31/12/2026");
 		System.out.println(date);
+		LOGGER.info("Formatted Date : {}", date);
+	}
+
+	public void displayCountry() throws Exception{
+		ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+		Country country = context.getBean("country", Country.class);
+
+
+		System.out.println(country.getCode());
+		System.out.println(country.getName());
 	}
 
 }
