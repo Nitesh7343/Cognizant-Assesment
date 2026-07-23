c91f5b8089219de61315e5115424c62aa8781381
Nitesh7343 <niteshsingh7343@gmail.com>
2026-07-23 15:41:10 +0530

    country class implemented

---
+++ Week_5/spring-learn/src/main/java/com/cognizant/spring_learn/model/Country.java
@@ -0,0 +1,35 @@
+package com.cognizant.spring_learn.model;
+
+import java.util.logging.Logger;
+
+public class Country {
+    private String code;
+    private String name;
+
+    Country() {
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
